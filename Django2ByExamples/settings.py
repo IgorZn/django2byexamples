696263997f533a080bbc224a1c9bf1d334ae750e
Igor Znamensky <igor.znamensky@gmail.com>
2019-08-26 21:56:42 +0300

    Гл. 2, Добавление продвинутых функций в блог

--- Django2ByExamples/settings.py
+++ Django2ByExamples/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'blog.apps.BlogConfig',
 ]
 
@@ -119,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
