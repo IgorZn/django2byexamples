6034d1d658148e0e350e8e77f860ffde9e0c981e
Igor Znamensky <iz@cargoonline.ru>
2019-08-31 15:21:17 +0300

    Гл.2, Формирование списка рекоменДованных статей, ст.69 "Теперь отредактируйте HTML"

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.views.generic import ListView
 
 from taggit.models import Tag
@@ -20,6 +21,11 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
     # List of active comments for particular post
     comments = post.comments.filter(active=True)
@@ -46,7 +52,8 @@ def post_detail(request, year, month, day, post):
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'new_comment': new_comment,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
