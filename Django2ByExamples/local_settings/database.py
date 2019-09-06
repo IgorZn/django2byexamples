58d064707214a26d54c3e51cacbb2931522301eb
Igor Znamensky <igor.znamensky@gmail.com>
2019-09-06 22:47:43 +0300

    Гл. 3, Стемминг и ранжирование результатов + local_settings

---
+++ Django2ByExamples/local_settings/database.py
@@ -0,0 +1,7 @@
+# --DATABASE SETTINGS, do not sync this file to git. This file should be ONLY LOCAL
+
+DB_ENGINE = "django.db.backends.postgresql"
+DB_ADDRESS = 'localhost'
+DB_USER = 'blog'
+DB_PASSWORD = 'password'
+DB_NAME = 'blog'
