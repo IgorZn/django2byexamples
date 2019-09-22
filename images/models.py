aa41f0f4ee6633301d5c4d02cb4a83c535ff5c61
Igor Znamensky <igor.znamensky@gmail.com>
2019-09-22 23:13:02 +0300

    Глава 5, Совместное использование содержимого сайта. использование изображений с Других сайтов

--- images/models.py
+++ images/models.py
@@ -19,13 +19,14 @@ class Image(models.Model):
 
     """
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE())
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
     images = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
+    user_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     def __str__(self):
         return self.title
