ba0ee62fb4a9fa8f16ce2ba3b86c58a7bdef3934
Igor Znamensky <igor.znamensky@gmail.com>
2019-10-24 23:02:32 +0300

    Глава 6, Отслеживание действий пользователей. Сохранение представлений объектов в Redis

--- Django2ByExamples/settings.py
+++ Django2ByExamples/settings.py
@@ -220,3 +220,7 @@ from django.urls import reverse_lazy
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('account:user_detail', args=[u.username])
 }
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
