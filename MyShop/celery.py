35a8f8144f5963dccad024261975901378d9284e
Igor Znamensky <igor.znamensky@gmail.com>
2019-11-21 23:09:16 +0300

    Глава 7, Создание онлайн-магазина Исправил проблему с очередью

--- MyShop/celery.py
+++ MyShop/celery.py
@@ -2,7 +2,8 @@ import os
 from celery import Celery
 
 # Задаем переменную окружения, содержащую название файла настроек нашего проекта
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyShop.settings')
+# os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
 app = Celery('MyShop')
 
