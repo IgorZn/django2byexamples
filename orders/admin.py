e8d41f4e78201c6462b9081accefc053472857fb
Igor Znamensky <igor.znamensky@gmail.com>
2019-12-06 00:10:52 +0300

    Глава 8, Управление заказами и платежами Экспорт заказов в CSV-файл

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,38 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
 
+
 # Register your models here.
+
+def export_to_csv(modeladmin, request, queryset):
+    print(queryset)
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment;filename={opts.verbose_name}.csv'
+    writer = csv.writer(response, dialect='excel')
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+
+    # Записываем первую строку с заголовками полей
+    writer.writerow([field.verbose_name for field in fields])
+
+    # Записываем данные
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        print(data_row)
+        writer.writerow(data_row)
+    return response
+
+export_to_csv.short_description = 'Export to CSV'
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -24,3 +55,4 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
